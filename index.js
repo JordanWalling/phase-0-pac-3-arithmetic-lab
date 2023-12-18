0facafcf5bd67e09f9b2986ad656d8f8666399b2
JordanWalling <jordanwalling1991@gmail.com>
2023-12-18 14:24:57 +1100

    complete lab

---
+++ index.js
@@ -0,0 +1,37 @@
+// Addition function
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract function
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiply function
+function multiply(a, b) {
+  return a * b;
+}
+// Divide function
+function divide(a, b) {
+  return a / b;
+}
+// increment by one
+function increment(a) {
+  return a + 1;
+}
+
+// decrement by one
+function decrement(a) {
+  return a - 1;
+}
+
+// parses as integer
+function makeInt(a) {
+  return parseInt(a, 10);
+}
+
+// parses as a floating point number
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
